bd3070b60422d01906338667315d6c35ba9adc9a
Timm Ruland <7198877+BlueCrescent@users.noreply.github.com>
2024-03-06 18:16:53 +0100

    feat(data): Added some error handling to TensorDataLoaderPipe.

--- src/docllm/data/pretraining/tensor_data_loader_pipe.py
+++ src/docllm/data/pretraining/tensor_data_loader_pipe.py
@@ -12,9 +12,23 @@ class TensorDataLoaderPipe(IterDataPipe):
 
     def __iter__(self) -> Iterable[Tuple[List[torch.Tensor], List[torch.Tensor]]]:
         for file_name in self._source_datapipe:
-            file_data = torch.load(file_name, map_location="cpu")
-            if len(file_data) > 0:
-                text_tokens, bbox_tokens = tuple(map(list, zip(*file_data)))
-                yield text_tokens, bbox_tokens
-            else:
-                logging.warning(f"File {file_name} is empty.")
+            try:
+                yield self._try_parse_file(file_name)
+            except Exception as e:
+                logging.warning(f"Error while parsing file {file_name}: {e}")
+
+    def _try_parse_file(self, file_name: str) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
+        file_data = torch.load(file_name, map_location="cpu")
+        if len(file_data) == 0:
+            raise ValueError(f"File is empty.")
+        text_tokens, bbox_tokens = tuple(map(list, zip(*file_data)))
+        self._check_data(text_tokens, bbox_tokens)
+        return text_tokens, bbox_tokens
+
+    def _check_data(self, text_tokens: List[torch.Tensor], bbox_tokens: List[torch.Tensor]) -> None:
+        if len(text_tokens) != len(bbox_tokens):
+            raise ValueError(
+                f"Length of text tokens ({len(text_tokens)}) and bbox tokens ({len(bbox_tokens)}) must match."
+            )
+        if not all(bbox_tokens[i].size() == (text_tokens[i].size()[0], 4) for i in range(len(text_tokens))):
+            raise ValueError("Unexpected bounding box shape.")
