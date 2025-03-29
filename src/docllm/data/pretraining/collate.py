6f73aede78e7e2f356231f1b96b556335e2efbb4
Timm Ruland <timm.ruland@uni-bonn.de>
2025-03-29 12:48:09 +0100

    feat(pretraining): Made collate function more flexible in its inputs.
    Also improved documentation.

--- src/docllm/data/pretraining/collate.py
+++ src/docllm/data/pretraining/collate.py
@@ -1,16 +1,29 @@
-from typing import List, Tuple
+from typing import List, Set, Tuple
 
 import torch
 from torch.nn.utils.rnn import pad_sequence
 
 
 def collate(
-    items: List[Tuple[torch.LongTensor, torch.FloatTensor, torch.BoolTensor, torch.LongTensor]],
-    padding_value: float,
-) -> Tuple[torch.LongTensor, torch.FloatTensor, torch.BoolTensor, torch.LongTensor]:
-    inputs, bboxes, mask, labels = zip(*items)
-    input_batch = pad_sequence(inputs, batch_first=True, padding_value=padding_value)
-    bbox_batch = pad_sequence(bboxes, batch_first=True, padding_value=padding_value)
-    mask_batch = pad_sequence(mask, batch_first=True, padding_value=padding_value)
-    labels_batch = pad_sequence(labels, batch_first=True, padding_value=padding_value)
-    return input_batch, bbox_batch, mask_batch, labels_batch
+    items: List[Tuple[torch.Tensor, ...]], padding_value: float, ignore_indices: Set[int] = {}
+) -> Tuple[torch.Tensor, ...]:
+    """Turns a list of tuples of tensors into a tuple of collated tensors.
+       The nth entry of each tuple in the list is collated together as the nth entry of the output tuple.
+
+    Args:
+        items (List[Tuple[torch.Tensor, ...]]): A list of tuples containing tensors to be collated.
+        padding_value (float): The value to be used for all padding. Probably zero.
+        ignore_indices (Set[int], optional): A set of indices to ignore. Defaults to {}.
+
+    Returns:
+        Tuple[torch.Tensor, ...]: A tuple of collated tensors.
+    """
+    return tuple(
+        collate_tensors(tensors, padding_value=padding_value)
+        for i, tensors in enumerate(zip(*items))
+        if i not in ignore_indices
+    )
+
+
+def collate_tensors(inputs: List[torch.Tensor], padding_value: float) -> torch.Tensor:
+    return pad_sequence(inputs, batch_first=True, padding_value=padding_value)
