16c80a2d9798feb8c400170ff547b0ddde49c6b5
Timm Ruland <timm.ruland@uni-bonn.de>
2025-03-29 12:49:16 +0100

    feat(data): Added logic for precomputing an epoch and loading this as a dataset.

---
+++ src/docllm/data/pretraining/precompute_epoch.py
@@ -0,0 +1,135 @@
+import logging
+import multiprocessing
+import os
+import random
+from typing import Iterable, List, Tuple
+
+import torch
+
+from docllm.data.pretraining.config import DocLLMPreTrainDataConfig
+from docllm.data.pretraining.masker import DocLLMPretrainingMasker
+
+
+def precompute_epoch_files(
+    directory: str, output_directory: str, config: DocLLMPreTrainDataConfig, num_processes: int
+) -> None:
+    files = list_pt_files(directory)
+    random.shuffle(files)
+    queue = multiprocessing.Queue()
+    for file in files:
+        queue.put(file)
+
+    workers = [
+        multiprocessing.Process(target=precompute_worker, args=(queue, output_directory, config, f"subset_{i}_file"))
+        for i in range(num_processes)
+    ]
+    try:
+        for worker in workers:
+            worker.start()
+        for _ in range(num_processes):
+            queue.put(None)
+    except Exception as e:
+        print(f"Error while precomputing files: {e}")
+        for worker in workers:
+            worker.terminate()
+        raise e
+    finally:
+        for worker in workers:
+            worker.join()
+
+
+def precompute_worker(
+    queue: multiprocessing.Queue, output_directory: str, config: DocLLMPreTrainDataConfig, prefix: str
+) -> None:
+    precomputer = SubsetPrecomputer(output_directory, config, prefix)
+    precomputer.process(IterableQueue(queue))
+
+
+class SubsetPrecomputer:
+    def __init__(self, output_directory: str, config: DocLLMPreTrainDataConfig, prefix: str):
+        self._output_directory = output_directory
+        self._max_sequence_length = config.max_seq_length
+        self._prefix = prefix
+        self._masker = DocLLMPretrainingMasker(config)
+        self._elements = []
+        self._total_size = 0
+
+    def process(self, files: Iterable[str]) -> None:
+        self._save_packed_data(self._build_packed_data(self._build_training_inputs(self._load_pickle_file(files))))
+
+    def _load_pickle_file(self, file_paths: Iterable[str]) -> Iterable[Tuple[List[torch.Tensor], List[torch.Tensor]]]:
+        for path in file_paths:
+            file_data = torch.load(path, map_location="cpu", weights_only=True)
+            if len(file_data) == 0:
+                print(f"File '{path}' is empty.")
+                continue
+            text_tokens, bbox_tokens = tuple(map(list, zip(*file_data)))
+            if self._check_data(text_tokens, bbox_tokens, path):
+                yield text_tokens, bbox_tokens
+
+    def _check_data(self, text_tokens: List[torch.Tensor], bbox_tokens: List[torch.Tensor], path: str) -> bool:
+        if len(text_tokens) != len(bbox_tokens):
+            logging.warning(
+                f"Length of text tokens ({len(text_tokens)}) and bbox tokens "
+                f"({len(bbox_tokens)}) must match. (In file {path})"
+            )
+            return False
+        if not all(bbox_tokens[i].size() == (text_tokens[i].size()[0], 4) for i in range(len(text_tokens))):
+            logging.warning("Unexpected bounding box shape. (In file {path})")
+            return False
+        return True
+
+    def _build_training_inputs(
+        self, input_and_bbox_tensors: Iterable[Tuple[List[torch.Tensor], List[torch.Tensor]]]
+    ) -> Iterable[Tuple[torch.LongTensor, torch.FloatTensor, torch.BoolTensor, torch.LongTensor]]:
+        for input_tensors, bbox_tensors in input_and_bbox_tensors:
+            try:
+                yield self._masker(input_tensors, bbox_tensors)
+            except ValueError as e:
+                logging.warning(e)
+
+    def _build_packed_data(
+        self, file_data: Iterable[Tuple[torch.LongTensor, torch.FloatTensor, torch.BoolTensor, torch.LongTensor]]
+    ) -> Iterable[Tuple[torch.LongTensor, torch.FloatTensor, torch.BoolTensor, torch.LongTensor]]:
+        for item in file_data:
+            item_size = item[0].size(0)
+            if self._total_size + item_size > self._max_sequence_length:
+                yield self._build_packed_entry(self._elements)
+                self._elements = []
+                self._total_size = 0
+            self._elements.append(item)
+            self._total_size += item_size
+
+        if self._elements:
+            yield self._build_packed_entry(self._elements)
+
+    def _build_packed_entry(
+        self, elemets: List[Tuple[torch.LongTensor, torch.FloatTensor, torch.BoolTensor, torch.LongTensor]]
+    ) -> Tuple[torch.LongTensor, torch.FloatTensor, torch.BoolTensor, torch.LongTensor]:
+        inputs, bboxes, mask, labels = zip(*elemets)
+        sizes = torch.LongTensor([i.size(0) for i in inputs])
+        return torch.cat(inputs), torch.cat(bboxes), torch.cat(mask), torch.cat(labels), sizes
+
+    def _save_packed_data(
+        self,
+        packed_data: Iterable[Tuple[torch.LongTensor, torch.FloatTensor, torch.BoolTensor, torch.LongTensor]],
+    ) -> None:
+        for i, data in enumerate(packed_data):
+            new_fn = os.path.join(self._output_directory, self._prefix + f"_{i}.pt")
+            torch.save(data, new_fn)
+
+
+class IterableQueue:
+    def __init__(self, queue: multiprocessing.Queue):
+        self.queue = queue
+
+    def __iter__(self):
+        while True:
+            task = self.queue.get()
+            if task is None:
+                break
+            yield task
+
+
+def list_pt_files(directory: str) -> List[str]:
+    return [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(".pt")]
---
+++ src/docllm/data/pretraining/precomputed_epoch_loader.py
@@ -0,0 +1,97 @@
+import os
+from functools import partial
+from typing import Dict, List, Tuple
+
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+from docllm.data.pretraining.collate import collate
+
+PackedData = Tuple[
+    torch.LongTensor, torch.FloatTensor, torch.BoolTensor, torch.LongTensor, torch.BoolTensor, torch.LongTensor
+]
+
+
+def build_epoch_dataloader(directory: str, batch_size: int, padding_value: float, offset_idx: int = 0) -> DataLoader:
+    dataset = PrecomputedEpoch(directory, offset_idx=offset_idx)
+    return torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, collate_fn=partial(collate_with_packing, padding_value=padding_value)
+    )
+
+
+class PrecomputedEpoch(Dataset):
+    def __init__(self, directory: str, offset_idx: int = 0):
+        self._directory = directory
+        self._file_names = [f for f in os.listdir(directory) if f.endswith(".pt")]
+        self._offset_idx = offset_idx
+
+    def __len__(self):
+        return len(self._file_names) + self._offset_idx
+
+    def __getitem__(self, idx: int) -> PackedData:
+        idx -= self._offset_idx
+        if idx < 0:
+            raise IndexError(f"Index {idx + self._offset_idx} is bellow given offset.")
+        file_path = os.path.join(self._directory, self._file_names[idx])
+        input_ids, bbox, loss_mask, labels, sizes = torch.load(file_path)
+
+        attention_mask, position_ids = compute_packing_attention_mask_and_pos_ids(sizes)
+
+        return input_ids, bbox, loss_mask, labels, position_ids, attention_mask
+
+
+def compute_packing_attention_mask_and_pos_ids(sizes: torch.LongTensor) -> Tuple[torch.BoolTensor, torch.LongTensor]:
+    total_len = sizes.sum()
+    mask = torch.zeros((total_len, total_len), dtype=torch.bool)
+    pos_ids = torch.zeros(total_len, dtype=torch.long)
+    start = 0
+    for size in sizes:
+        end = start + size
+        mask[start:end, start:end] = torch.tril(torch.ones((size, size), dtype=torch.bool))
+        pos_ids[start:end] = torch.arange(size)
+        start = end
+    return mask, pos_ids
+
+
+def collate_as_dict(
+    inputs: List[PackedData], padding_value: float, additional_col: bool = False
+) -> Dict[str, torch.Tensor]:
+    input_ids, bbox, loss_mask, labels, position_ids, attention_mask = collate_with_packing(
+        inputs, padding_value, additional_col
+    )
+    return {
+        "input_ids": input_ids,
+        "input_coordinates": bbox,
+        "loss_mask": loss_mask,
+        "labels": labels,
+        "position_ids": position_ids,
+        "attention_mask": attention_mask,
+    }
+
+
+def collate_with_packing(inputs: List[PackedData], padding_value: float, additional_col: bool = False) -> PackedData:
+    att_mask_idx = 5
+    res = collate(inputs, padding_value, ignore_indices={att_mask_idx})
+    input_ids, bbox, loss_mask, labels, position_ids = res
+    max_length = input_ids.size(1)
+    padded_attention_masks = torch.stack(
+        [_pad_attention_mask(t[att_mask_idx], max_length, additional_col) for t in inputs]
+    )
+    assert padded_attention_masks.size(1) == max_length
+    assert padded_attention_masks.size(2) == max_length + 1
+    return input_ids, bbox, loss_mask, labels, position_ids, padded_attention_masks
+
+
+def _pad_attention_mask(mask: torch.BoolTensor, to_length: int, additional_col: bool) -> torch.BoolTensor:
+    # if mask.size(0) == to_length:
+    #     return mask
+    return torch.cat(
+        [
+            torch.cat([mask, torch.zeros(to_length - mask.size(0), mask.size(1), device=mask.device)], dim=0),
+            torch.zeros(to_length, to_length - mask.size(1) + int(additional_col), device=mask.device),
+        ],
+        dim=1,
+    )
+
+
+# attention_mask = torch.cat([attention_mask, torch.zeros(2, attention_mask.size(1), 1, dtype=torch.bool)], dim=2)
