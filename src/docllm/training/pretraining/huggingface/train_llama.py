980e760801631b5ee280e69675a4c35e9425c4ee
Timm Ruland <timm.ruland@uni-bonn.de>
2025-04-06 22:21:05 +0200

    refactor(pretraining): Cleanup of llama based training code.

--- src/docllm/training/pretraining/huggingface/train_llama.py
+++ src/docllm/training/pretraining/huggingface/train_llama.py
@@ -1,5 +1,4 @@
 import os
-import pickle
 from functools import partial
 from typing import Dict, Iterable, List, Optional, Tuple
 
@@ -31,7 +30,6 @@ class LlamaDocLLMTrainerConfig(BaseModel):
     checkpoint: str
     batch_size: int
     num_processes: int = 4
-    # use_data_pipeline: bool = False
     data_format: DataFormat
     train_data_dir: str
     eval_data_dir_in: str
@@ -191,17 +189,8 @@ class LlamaDocLLMTrainer:
             include_num_input_tokens_seen=False,  # FIXME: reactivate when this is merged: https://github.com/huggingface/transformers/pull/34554
             report_to=["tensorboard"],
             fsdp="full_shard" if self._config.use_fsdp else "",  # full_shard offload auto_wrap",  # FIXME
-            # fsdp_min_num_params=int(1e2),
             fsdp_config=(
-                {
-                    # "min_num_params": int(1e3),
-                    "transformer_layer_cls_to_wrap": [DocLLMLlamaDecoderLayer.__name__],
-                    # "backward_prefetch": "backward_pre",
-                    # "forward_prefetch": False,
-                    # "limit_all_gathers": False,
-                }
-                if self._config.use_fsdp
-                else {}
+                {"transformer_layer_cls_to_wrap": [DocLLMLlamaDecoderLayer.__name__]} if self._config.use_fsdp else {}
             ),
         )
 
@@ -249,63 +238,7 @@ def collate_data(data: List[Dict[str, torch.Tensor]]) -> Dict[str, torch.Tensor]
     }
 
 
-# class CustomTrainer(Trainer):
-#     def on_step(self, args, state, control, **kwargs):
-#         # Compute the gradient norm
-#         grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1e6, norm_type=2)
-
-#         # Check for NaN in the gradient norm
-#         if grad_norm.isnan():
-#             # Retrieve the current batch
-#             dataloader = self.get_train_dataloader()
-#             current_batch = next(iter(dataloader))
-
-#             # Log information
-#             print(f"Gradient norm is NaN at step {state.global_step}")
-#             print(f"Batch index: {state.global_step}")
-#             print(f"Batch content: {current_batch}")
-
-#             # Assuming you have access to the dataset indices
-#             batch_indices = current_batch["input_ids"]  # or the specific key used in your dataset
-#             samples = [self.train_dataset[i] for i in batch_indices]
-#             print(f"Batch samples: {samples}")
-
-#         super().on_step(args, state, control, **kwargs)
-
-
-# class GradientNormCallback(TrainerCallback):
-#     def on_log(self, args, state, control, logs=None, **kwargs):
-#         # _ = logs.pop("total_flos", None)
-#         if state.is_local_process_zero:
-#             # Get the logged gradient norm and loss
-#             grad_norm = logs.get("gradient_norm", None)
-#             loss = logs.get("loss", None)
-
-#             # Check if gradient norm is NaN
-#             if grad_norm is not None and torch.isnan(torch.tensor(grad_norm)):
-#                 print(logs)
-#                 # Retrieve the current batch
-
-#                 # Log pertinent information
-#                 print(f"Gradient norm is NaN at step {state.global_step}")
-#                 print(f"Loss: {loss}")
-#                 print(f"Batch index: {state.global_step}")
-#                 if dataloader := kwargs.get("train_dataloader", None):
-#                     current_batch = next(iter(dataloader))
-#                     print(f"Batch content: {current_batch}")
-
-#                 # Assuming you have access to the dataset indices
-#                 # batch_indices = current_batch['input_ids']  # or the specific key used in your dataset
-#                 # samples = [self.train_dataset[i] for i in batch_indices]
-#                 # print(f"Batch samples: {samples}")
-#             # print(logs)
-
-
 class SkipNaNGradientsCallback(TrainerCallback):
-    # def on_step_begin(self, args, state: TrainerState, control: TrainerControl, **kwargs):
-    #     local_rank = int(os.environ.get("LOCAL_RANK", -1))
-    #     print(f">>>>>({local_rank}) Step: {state.global_step}", flush=True)
-
     def on_pre_optimizer_step(self, args, state: TrainerState, control: TrainerControl, **kwargs):
         model: PreTrainedModel | torch.nn.Module | None = kwargs.get("model", None)
         if model is None:
@@ -337,103 +270,3 @@ class SkipNaNGradientsCallback(TrainerCallback):
         for name, param in model.named_parameters():
             if param.grad is not None:
                 param.grad.data.zero_()
-
-
-class NaNGradientDetectionCallback(TrainerCallback):
-    def __init__(self, log_file="", output_dir=""):
-        self.log_file = log_file
-        self.output_dir = output_dir
-        self.batch = None
-        self.batch_index = None
-        self.loss = None
-        self.stop_training = False
-        self.substep_counter = 0
-
-    def on_train_begin(self, args, state, control, **kwargs):
-        local_rank = int(os.environ.get("LOCAL_RANK", -1))
-        print(f"Local Rank: {local_rank}")
-        if local_rank == 0:
-            world_size = int(os.environ.get("WORLD_SIZE", -1))
-            print(f"World Size: {world_size}")
-
-    def on_step_begin(self, args, state: TrainerState, control: TrainerControl, **kwargs):
-        self.batch_index = state.global_step
-        # self.batch = kwargs.get("batch")
-        print(f"Batch index: {self.batch_index}", flush=True)
-        print(f"Substep counter: {self.substep_counter}", flush=True)
-        local_rank = int(os.environ.get("LOCAL_RANK", -1))
-        print(f"Local Rank: {local_rank}")
-        self.substep_counter = 0
-
-    def on_substep_end(self, args, state: TrainerState, control: TrainerControl, **kwargs):
-        self.substep_counter += 1
-
-    def on_step_end(self, args, state: TrainerState, control: TrainerControl, **kwargs):
-        self.loss = kwargs.get("loss")
-
-    def on_pre_optimizer_step(self, args, state: TrainerState, control: TrainerControl, **kwargs):
-        model = kwargs.get("model", None)
-        # optimizer = kwargs.get("optimizer", None)
-        # if not state.is_local_process_zero:
-        #     return
-        local_rank = int(os.environ.get("LOCAL_RANK", -1))
-        if model is None:
-            print(f"\n\n>>>>>({local_rank}) Model not available in on_pre_optimizer_step.", flush=True)
-            return
-        for name, param in model.named_parameters():
-            if param.grad is not None:
-                grad_norm = param.grad.data.norm()
-                if torch.isnan(grad_norm):
-                    print(f"\n\n>>>>>on_pre_optimizer_step({local_rank})<<<<<\n\n")
-                    # with open(self.log_file, "a") as f:
-                    print(f"NaN gradient detected at batch index {self.batch_index}\n", flush=True)
-                    print(f"Loss: {self.loss}\n", flush=True)
-                    # print(f"Batch content: {self.batch}\n", flush=True)
-                    # Sauvegarde le batch en tant que fichier pickle
-                    # try:
-                    #     torch.save(self.batch, f"debug_batch_{self.batch_index}.pt")
-                    # except Exception as e:
-                    #     print(f"Error saving batch: {e}", flush=True)
-                    # with open(f"debug_batch_{self.batch_index}.pkl", "wb") as f:
-                    #     pickle.dump(self.batch, f)
-                    # Sauvegarde un checkpoint du modèle et de l'optimiseur
-                    # output_dir = f"checkpoint_nan_grad_batch_{self.batch_index}"
-                    # model.save_pretrained(output_dir)
-                    # torch.save(optimizer.state_dict(), f"{output_dir}/optimizer.pt")
-                    # Termine l'entraînement
-                    # control.should_training_stop = True
-                    self.stop_training = True
-                    break
-
-    def on_log(self, args, state: TrainerState, control: TrainerControl, logs=None, **kwargs):
-        # _ = logs.pop("total_flos", None)
-        # if state.is_local_process_zero:
-        # Get the logged gradient norm and loss
-        grad_norm = logs.get("gradient_norm", None)
-        loss = logs.get("loss", None)
-        local_rank = int(os.environ.get("LOCAL_RANK", -1))
-
-        # Check if gradient norm is NaN
-        grad_is_nan = grad_norm is not None and torch.isnan(torch.tensor(grad_norm))
-        if grad_is_nan or self.stop_training:
-            print(f"\n\n>>>>>on_log({local_rank})<<<<<\n\n")
-            print(f"({local_rank}){grad_is_nan=}")
-            print(f"logs({local_rank}):\n")
-            print(logs, flush=True)
-            print(f"\n\nstate({local_rank}):\n")
-            print(state, flush=True)
-            print("\n\n")
-            # Retrieve the current batch
-
-            # Log pertinent information
-            print(f"({local_rank})Gradient norm is NaN at step {state.global_step}")
-            print(f"({local_rank})Loss: {loss}")
-            print(f"({local_rank})Batch index: {state.global_step}", flush=True)
-            if dataloader := kwargs.get("train_dataloader", None):
-                current_batch = next(iter(dataloader))
-                print(f"({local_rank})Batch content: {current_batch}", flush=True)
-                try:
-                    torch.save(current_batch, f"debug{local_rank}_batch_{self.batch_index}.pt")
-                except Exception as e:
-                    print(f"({local_rank})Error saving batch: {e}", flush=True)
-            control.should_training_stop = True
